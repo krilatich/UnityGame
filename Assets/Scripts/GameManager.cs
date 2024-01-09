e6e20234cac4df6668d87b122a358f5376e3cc78
krilatich <106861958+krilatich@users.noreply.github.com>
2024-01-09 17:58:08 +0700

    final commit

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,67 +1,154 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
-using UnityEngine.UI;
-using System.Text;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    //canvas object
-    public GameObject parentObject;
-
-    //prefab object
-    public Image building;
-
-    [Range(1, 10)]
-    public int gridX, gridY;
-
-    [SerializeField]
-    private float buildingCurrentSizeX, buildingCurrentSizeY;
-
-    [SerializeField]
-    private float defaultPositionX, defaultPositionY;
-
-    public Sprite selectedSprite;
-    public int selectedCost;
-
-    public Text balanceText;
-    public int balance;
-    
-    void Start()
+    private static GameManager gameManagerInstance;
+    public static GameManager Instance
     {
-        PlayerPrefs.DeleteAll();
-        
-        buildingCurrentSizeX = building.GetComponent<RectTransform>().sizeDelta.x;
-        buildingCurrentSizeY = building.GetComponent<RectTransform>().sizeDelta.y;
-
-        defaultPositionX = building.GetComponent<RectTransform>().anchoredPosition.x;
-        defaultPositionY = building.GetComponent<RectTransform>().anchoredPosition.y;
-
-        GridSystem();
-    }
-
-    void GridSystem()
-    {
-        for(int i = 0; i < gridX; i++)
+        get
         {
-            for(int j = (gridY - 1); j >=0; j--)
-            {
-                Image image = Instantiate(building, parentObject.transform);
-                image.GetComponent<RectTransform>().anchoredPosition = new Vector3(defaultPositionX + (buildingCurrentSizeX * i), defaultPositionY - (buildingCurrentSizeY * j), 0);
+            if (gameManagerInstance == null)
+                gameManagerInstance = FindObjectOfType<GameManager>();
 
-                var buildingName = new StringBuilder();
-                buildingName.Append("Building");
-                buildingName.Append(i);
-                buildingName.Append("_");
-                buildingName.Append(j);
-
-                image.name = buildingName.ToString();
-            }
+            return gameManagerInstance;
         }
     }
 
-    void Update()
+    public const int MaxLives = 5;
+    public int InitialMoney;
+
+    public int Level;
+    public GameObject VictoryText;
+    public GameObject GameOverText;
+
+    public int InitialTurretPrice;
+    public int InitialRocketPrice;
+    public int TurretPriceAddition;
+    public int RocketPriceAddition;
+
+    private int turretPrice;
+    private int rocketPrice;
+
+    public static int Lives;
+    private int money;
+    private HealthDrawerScript healthDrawer;
+    private MoneyDrawer moneyDrawer;
+
+    private int remainingEnemies;
+
+    // Use this for initialization
+    void Start ()
     {
-        balanceText.text = balance.ToString();
+        money = InitialMoney;
+
+        turretPrice = InitialTurretPrice;
+        rocketPrice = InitialRocketPrice;
+
+        healthDrawer = GetComponent<HealthDrawerScript>();
+        moneyDrawer = GetComponent<MoneyDrawer>();
+
+        moneyDrawer.Draw(InitialMoney);
+
+        remainingEnemies = GetComponent<EnemySpawner>().Waves.Sum(w => w.Amount);
+    }
+
+    public void EnemyEscaped(GameObject enemy)
+    {
+        Lives--;
+        CameraShaker.Instance.Shake();
+        healthDrawer.Draw(Lives);
+
+        if (Lives <= 0)
+        {
+            GameOver();
+        }
+
+        remainingEnemies--;
+        if(remainingEnemies == 0) Victory();
+    }
+
+    public void EnemyKilled(GameObject enemy)
+    {
+        remainingEnemies--;
+        if(remainingEnemies == 0) Victory();
+    }
+
+    public int GetMoney()
+    {
+        return money;
+    }
+
+    public void AddMoney(int value)
+    {
+        money += value;
+        moneyDrawer.Draw(money);
+    }
+
+    public void TurretBuilt(GameObject turret)
+    {
+        if (turret.CompareTag("turretTower"))
+        {
+            money -= turretPrice;
+            turretPrice += TurretPriceAddition;
+        }
+        else
+        {
+            money -= rocketPrice;
+            rocketPrice += RocketPriceAddition;
+        }
+
+        moneyDrawer.Draw(money);
+    }
+
+    public void CoinCollected(GameObject coin)
+    {
+        money += CoinScript.Value;
+        moneyDrawer.Draw(money);
+    }
+
+    public bool EnoughMoneyForTurret(string tag)
+    {
+        if(tag == "turretTower")
+            return money >= turretPrice;
+
+        return money >= rocketPrice;
+    }
+
+    public int MoneyForTurret(string tag)
+    {
+        return tag == "turretTower" ? turretPrice : rocketPrice;
+    }
+
+    public void Victory()
+    {
+        VictoryText.SetActive(true);
+        Invoke("NextLevel", 5.0f);
+    }
+    
+    public void GameOver()
+    {
+        GameOverText.SetActive(true);
+        Invoke("BackToMainMenu", 5.0f);
+    }
+
+    public void NextLevel()
+    {
+        if (Level <= 2)
+        {
+            SceneManager.LoadScene("Level_0" + (Level + 1));
+        }
+        else
+        {
+            SceneManager.LoadScene("Menu_screen");
+        }
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene("Menu_screen");
     }
 }
